5618c652eeb3fe0ef1edaf8fb5ac04f1655fc213
Oscar Chevalier <oscar@frozenmilk.dev>
2025-11-24 21:42:04 +1100

    chore: add Sloth's Load plugin

--- KotlinExamples/build.gradle.kts
+++ KotlinExamples/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.3.0-0.1.4"
+    id("dev.frozenmilk.teamcode") version "10.3.0-0.1.4"
+    id("dev.frozenmilk.sinister.sloth.load") version "0.2.4"
 }
 // Most FTC libraries will want the following
 ftc {
-	kotlin // if you don't want to use kotlin, remove this
+    kotlin // if you don't want to use kotlin, remove this
 }
 
 repositories {
